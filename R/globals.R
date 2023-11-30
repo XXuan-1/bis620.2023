b66b557e1a09122f61fdc547e46dd3e79b012b00
XXuan-1 <jiaxuansun@ucsb.edu>
2023-11-29 19:49:24 -0500

    fixed error.

--- R/globals.R
+++ R/globals.R
@@ -2,5 +2,4 @@
 utils::globalVariables(c("X", "Y", "Z", "Acceleration", "studies",
                          "source_class", "max_num_studies", "nct_id",
                          "designs", "intervention_model", "allocation",
-                         "phase", "conditions", "name", "condition", "design",
-                         ))
+                         "phase", "conditions", "name", "condition", "design"))
